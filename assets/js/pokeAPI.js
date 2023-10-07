896759aef7b2b00b8f7e0d3ebb9c23c8116e6d06
LuizaAMaia <araujomluiza@gmail.com>
2023-10-07 05:57:17 -0300

    atulizaçao com erro

--- assets/js/pokeAPI.js
+++ assets/js/pokeAPI.js
@@ -1,8 +1,26 @@
+
 const pokeAPI = {}
+
+function convertPokeAPIDetailToPokemon(pokeDetail) {
+    const pokemon = new Pokemon()
+    pokemon.number = pokeDetail.order
+    pokemon.name = pokeDetail.name
+
+    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
+    const [type] = types
+
+    pokemon.types = types
+    pokemon.type = type
+
+    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+
+    return pokemon
+}
 
 pokeAPI.getPokemonDetail = (pokemon) => {
     return fetch(pokemon.url)
             .then((response) => response.json())
+            .then(convertPokeAPIDetailToPokemon)
 }
 
 pokeAPI.getPokemons = (offset = 0, limit = 10) => {
@@ -14,5 +32,5 @@ pokeAPI.getPokemons = (offset = 0, limit = 10) => {
         .then((pokemons) => pokemons.map(pokeAPI.getPokemonDetail))
         .then((detailRequest) => Promise.all(detailRequest))
         .then((pokemonDetails) => pokemonDetails)
-        .catch((error) => console.error(error))
+        // .catch((error) => console.error(error))
 }
